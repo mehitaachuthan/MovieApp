b3a54eda4ddf8a64b7dae449d577092274f34cd4
mehitaachuthan <z.quanticle@gmail.com>
2020-06-23 19:08:18 -0700

    Make card and detailed page layout to display movies information

---
+++ app/src/main/java/test/example/movieapp/MainActivity.java
@@ -0,0 +1,15 @@
+package test.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
